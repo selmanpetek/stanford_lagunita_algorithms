0c7fe5038df68163291e873c15abc6e08ea59852
Selman <yedimseni67@hotmail.com>
2019-12-02 17:02:02 -0600

    minor improvements

--- merge_sort.py
+++ merge_sort.py
@@ -1,28 +1,23 @@
 def merge(matrix, a, b, mid):
     k = a
     l = mid + 1
-    c = 0
     temp = []
     while k < mid + 1 and l < b + 1:
         if matrix[k] < matrix[l]:
             temp.append(matrix[k])
-            c += 1
             k += 1
         else:
             temp.append(matrix[l])
             l += 1
-            c += 1
     while k < mid + 1:
         temp.append(matrix[k])
-        c += 1
         k += 1
     while l < b + 1:
         temp.append(matrix[l])
-        c += 1
         l += 1
 
     i = 0
-    while i < c:
+    while i <= b - a:
         matrix[a + i] = temp[i]
         i += 1
 
